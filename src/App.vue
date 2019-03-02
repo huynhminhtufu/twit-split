3189c229e11747efe998af31ad6f102658431aac
Tu Huynh <tu.huynh@mobiclixgroup.com>
2019-03-02 14:00:06 +0700

    build up app

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,17 @@
   <v-app>
     <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>ZALORA</span>
+        <span class="font-weight-light">TWITSPLIT</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+      <v-btn flat href="https://mrhmt.com" target="_blank">
+        <span class="mr-2">Tu Huynh's Blog</span>
       </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld />
+      <router-view />
     </v-content>
   </v-app>
 </template>
-
-<script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  },
-  data() {
-    return {
-      //
-    };
-  }
-};
-</script>
